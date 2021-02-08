01eed6d5347415f7e8503bc1bdcc7327fdf16aa6
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-02-08 17:51:29 -0300

    toques pessoais

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -6,15 +6,15 @@ import { ReactComponent as LinkedinIcon} from './linkedin.svg';
 function Footer () {
     return (
         <footer className="main-footer">
-            App desenvolvido durante a 2º ed. do evento Semana DevSuperior 
+            App desenvolvido durante a 2º ed. do evento Semana DevSuperior por VINICIUS GIACOMELLI 
             <div className="footer-icons">
-                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                <a href="https://www.youtube.com" target="_new">
                     <YoutubeIcon/>
                 </a>
-                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                <a href="https://www.linkedin.com/in/vinicius-giacomelli/" target="_new">
                     <LinkedinIcon/>
                 </a>
-                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+                <a href="https://www.instagram.com/vinygiacomelli/" target="_new">
                     <InstagramIcon/>
                 </a>
             </div>
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -10,7 +10,7 @@ function Home() {
                 <div className="home-content">
                     <div className="home-actions">
                         <h1 className="home-title">
-                            Faça seu pedido,<br /> e entregaremos <br /> para você <br />
+                            Cabeças pequenas,<br /> grandes ideias!  <br />
                         </h1>
                         <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br /> levaremos até sua porta.
